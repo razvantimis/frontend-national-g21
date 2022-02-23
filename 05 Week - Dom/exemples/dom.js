f5507cf5538adc89908d1399cff409f2e3a9cdf8
Razvan Timis <razvan.timis@8x8.com>
2022-02-23 20:04:08 +0200

    Add

---
+++ 05 Week - Dom/exemples/dom.js
@@ -0,0 +1,76 @@
+
+// CRUD - create, read, update, delete
+
+// R - Read html elements
+// Cum putem aducem in javascript un element din html?
+// Cautam dupa un element html
+// https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement
+function readHtmlElements() {
+  var sectionHTMLById = document.getElementById("firstSectionId");
+  console.log(sectionHTMLById)
+
+  var sectionHTMLByClass = document.getElementsByClassName('firstClass') // un array de elemente
+  console.log(sectionHTMLByClass[0])
+
+  // primul element gasit dupa regula de css selector
+  var sectionHTMLByQuery = document.querySelector('#firstSectionId.firstClass');
+  console.log(sectionHTMLByQuery)
+
+  // cautam dupa toate elementele
+  const sections = document.querySelectorAll('section'); // un array de elemente
+  console.log(sections);
+}
+// C - create html elements
+function createHtmlElments() {
+  // il creaza doar in memoria js
+  var myElement = document.createElement('h3');
+
+  // putem pune continut
+  // myElement.textContent = "Hello world"
+  myElement.innerHTML = "<b>Hello</b> world"
+
+  // inline style
+  myElement.style.backgroundColor = "red";
+  myElement.style.padding = "10px";
+
+  // adaugam myElement in header ca si children
+  // cautam element unde dorim sa adaugam
+  var headerHtml = document.querySelector('header');
+  headerHtml.appendChild(myElement)
+  // in body
+  document.body.appendChild(myElement)
+}
+
+// Update
+function updateHtmlElements(){
+  var headerHtml = document.querySelector('header');
+
+  // inline style
+  headerHtml.style.color = 'red'
+
+  // add class
+  headerHtml.classList.add('curs');
+
+  // replace de content
+  // headerHtml.textContent = "" // stergem tot din interior la header
+  headerHtml.innerHTML = "<nav> <ul> <li> item 1</li> </ul> </nav>"
+}
+
+// updateHtmlElements();
+
+// Delete
+// Se sterge doar elementul selectat
+// De unde stergm si ce stergem
+function deleteHtmlElements(){
+  var headerHtml = document.querySelector('header'); // vom sterge headr-ul
+  // de unde stergem? din <body>
+
+  // ca sa stergem avem nevoie de parintele elementului + elementul care va fi sters
+  // document.body.removeChild(headerHtml);
+
+  // cum aflam parintele? headerHtml.parentElement
+  headerHtml.parentElement.removeChild(headerHtml)
+
+}
+
+deleteHtmlElements()
