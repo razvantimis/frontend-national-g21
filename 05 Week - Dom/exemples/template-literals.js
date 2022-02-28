e8f70b8e11f3858ff262acf2186de2cdee7fad2a
Razvan Timis <razvan.timis@8x8.com>
2022-02-28 19:21:58 +0200

    Add
    Co-authored-by: berhecielena <berhecielena@users.noreply.github.com>
    Co-authored-by: Simona Popa <SimPopa@users.noreply.github.com>
    Co-authored-by: irinaeris <irinaeris@users.noreply.github.com>
    Co-authored-by: DaniiS97 <DaniiS97@users.noreply.github.com>
    Co-authored-by: dimusz <dimusz@users.noreply.github.com>
    Co-authored-by: Alexandru Țicău <neverbesaved@users.noreply.github.com>
    Co-authored-by: Radu Ionescu <radionesh@users.noreply.github.com>
    Co-authored-by: Alexandru Mihailescu <malexino@users.noreply.github.com>
    Co-authored-by: Pușcaș David - Cristian <David93Cluj@users.noreply.github.com>
    Co-authored-by: VladTamas20 <VladTamas20@users.noreply.github.com>
    Co-authored-by: Bogdan Muntean <bogdan-muntean@users.noreply.github.com>
    Co-authored-by: florinpop866 <florinpop866@users.noreply.github.com>
    Co-authored-by: Alina <alina-p17@users.noreply.github.com>

---
+++ 05 Week - Dom/exemples/template-literals.js
@@ -0,0 +1,42 @@
+// template literals
+
+// 1. putem scrie string pe mai multe
+const value1 = `string asdsadasdas loremasd
+
+test
+ce faci
+buna
+
+`
+
+console.log(value1)
+
+// 2. ne ajuta sa 
+
+// Exemple: Student: Cosmin | Varsta: 14 | Sex: M | CNP: 1960214234045
+
+var studentName = 'Cosmin'
+var age = 14;
+var sex = 'M'
+var cnp = '1960214234045'
+
+
+var result = "Student: " + studentName + " | Varsta:" + age + " | Sex:" + sex + " | CNP: " + cnp;
+
+var result2 = `Student: ${studentName} | Varsta: ${age + 10} | Sex: ${sex} | CNP: ${cnp}`
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
