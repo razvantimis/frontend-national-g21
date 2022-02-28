a43f1327db0bb0461e44c11aa1dea4dda7a9394a
Razvan Timis <razvan.timis@8x8.com>
2022-02-28 20:02:18 +0200

    Add

--- 05 Week - Dom/workshop/app.js
+++ 05 Week - Dom/workshop/app.js
@@ -7,16 +7,28 @@
 
 
 //  1. Sa afisam o lista de TODO items
+// <ul> <li>item 1 </li>   </ul>
 var todoList = ["cumparaturi", "plata facturi", "scoate cainele afara"];
-console.log('here')
+
+var ulHTML = document.createElement("ul");
 for (var i = 0; i < todoList.length; i++) {
     var item = todoList[i];
     var itemHTML = document.createElement("li");
-    itemHTML.innerHTML = item; document.body.appendChild(itemHTML)
+    itemHTML.innerHTML = item;
+    ulHTML.appendChild(itemHTML)
 }
+// il punem in body
+document.body.appendChild(ulHTML)
 
+// 2. Sa permitem adaugarea unui TODO
+// avem nevoie de un input si un button
+var toDoInput = document.getElementById("to-do-input");
+var toDoButton = document.getElementById("to-do-button");
 
-
+toDoButton.onclick = function () {
+    console.log("se da click!")
+    console.log (toDoInput.value)
+}
 
 
 
