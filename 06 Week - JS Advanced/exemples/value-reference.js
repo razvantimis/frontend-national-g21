c8bd0152975544e4f8160c747e4f6f03ada48bb4
Razvan Timis <razvan.timis@8x8.com>
2022-03-02 19:37:08 +0200

    Add

--- 06 Week - JS Advanced/exemples/value-reference.js
+++ 06 Week - JS Advanced/exemples/value-reference.js
@@ -37,11 +37,11 @@ console.log(studentsRO === studentsUS) // compara doar adresa de memorie / refer
 // parametru students - va primi referinta de la studentsRO
 // -  daca ar primi o copia a valorilor din studentsRO atunci am ramane fara memorie ram
 // Daca am trimit array prin copie sar dubla spatiu pentru array nostru
-function searchStudent(name, students){
-   name = 'Alex' // afectam copia lui studentName, nu afectam studentName din afara
+function searchStudent(name, students) {
+  name = 'Alex' // afectam copia lui studentName, nu afectam studentName din afara
 
-   console.log(students[0])
-   students[0] = 10; // va schimba array si infara functie deoarece avem referinta
+  console.log(students[0])
+  students[0] = 10; // va schimba array si infara functie deoarece avem referinta
 }
 
 // 100gb de date
@@ -51,6 +51,63 @@ searchStudent(studentName, studentsRO);
 
 console.log(studentName); // valoare va fi 'Razvan'
 console.log(studentsRO); // [10]
+
+
+// Object
+
+// parametru myStudent se trimite prin reference
+function exempleReferenceObject(myStudent) {
+  console.log(myStudent)
+
+  // adaugam o nou pririetate pe object
+  myStudent.age = 30
+}
+
+var student = {
+  name: 'Alex',
+  country: 'RO'
+}
+exempleReferenceObject(student)
+
+console.log(student); // va apare aici age: 30 ??
+
+
+// Old way - cea mai recomandata de folosit
+var produs4 = {
+  pret: 3,
+  cantitate: 15,
+  name: "Biscuiti",
+}
+// Este o functie pura - nu are side effects
+function getProductTotalToPay(produs){
+  console.log(produs)
+  var total = produs.pret * produs.cantitate;
+  return total;
+}
+
+produs4.total = getProductTotalToPay(produs4);
+
+
+// New way - nu recomand folosirea acestei metode
+var produs4 = {
+  pret: 3,
+  cantitate: 15,
+  name: "Biscuiti",
+}
+// Cum putem scoate o valoare din functie fara sa folosim return?
+function getProductTotalToPay(produs) {
+  console.log(produs)
+  var total = produs.pret * produs.cantitate;
+
+  produs.total = total; // va fi vizit in exteriorul functie
+  // este un side effects al functie 
+}
+
+
+var total = getProductTotalToPay(produs4)
+console.log(total) // functia nu mai returneaza nimic, total = undefined
+console.log(produs4.total)
+
 
 
 
