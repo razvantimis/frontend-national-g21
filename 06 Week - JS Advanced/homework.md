4d2fe9e1e9868f2de8544d93eba03520e1b3db58
Razvan Timis <razvan.timis@8x8.com>
2022-03-07 20:59:41 +0200

    Add Co-authored-by: Alexandru Țicău <neverbesaved@users.noreply.github.com> Co-authored-by: Alina <alina-p17@users.noreply.github.com> Co-authored-by: DaniiS97 <DaniiS97@users.noreply.github.com> Co-authored-by: florinpop866 <florinpop866@users.noreply.github.com> Co-authored-by: Alexandru Mihailescu <malexino@users.noreply.github.com> Co-authored-by: Radu Ionescu <radionesh@users.noreply.github.com> Co-authored-by: berhecielena <berhecielena@users.noreply.github.com> Co-authored-by: MomeaAlexandru <MomeaAlexandru@users.noreply.github.com> Co-authored-by: dimusz <dimusz@users.noreply.github.com> Co-authored-by: Bogdan Muntean <bogdan-muntean@users.noreply.github.com> Co-authored-by: VladTamas20 <VladTamas20@users.noreply.github.com> Co-authored-by: AndreeaFene <AndreeaFene@users.noreply.github.com>

--- 06 Week - JS Advanced/homework.md
+++ 06 Week - JS Advanced/homework.md
@@ -3,8 +3,7 @@
 1.  Definiti un container de dimensiuni fixe ( de exemplu height: 400, width: 400) unde puteti playerul - il numim gameContainer
 2.  Player sa poate misca in toate directile ( incepeti doar cu o directie )
 3.  Un player nu poate iesi inafara gameContainer ( de la punctul 1)
-4.  Adaugati abstacole, puneti cateva div-uri ( dimensiuni fixe cu anumita culoare) in gameContainer, pot fi random puse in container. 
-5.  Player daca intalneste un obstacol, nu poate trece de el
+4.  Adaugati abstacole, puneti cateva div-uri ( dimensiuni fixe cu anumita culoare) in gameContainer, pot fi random puse in mapa jocuri. 
 
 # Tips
 
@@ -19,5 +18,6 @@
 3. Folositi if-uri
 
 ## Optional
+0. Player daca intalneste un obstacol, nu poate trece de el +1
 1. Adaugam numar de vieti la player ( +1 activitate )
-2. Sa adaugam obstacole care scad din viata
+2. Sa adaugam obstacole care scad din viata +1
--- 06 Week - JS Advanced/workshop/player.js
+++ 06 Week - JS Advanced/workshop/player.js
@@ -12,13 +12,26 @@ document.addEventListener("keydown", function (event) {
   console.log("event= ", event)
   if (event.code === "ArrowDown") {
     let oldTop = parseInt(player.style.top)
-    player.style.top = `${oldTop + 10}px`
+    if (oldTop < 370) {
+      player.style.top = `${oldTop + 10}px`
+    }
   }
   if (event.code === "ArrowRight") {
-    let oldLeft = parseInt(player.style.left) // daca style.ight nu exista avem NaN
-    player.style.left = `${oldLeft + 10}px` // NaN + 12 => NaN
+    let oldLeft = parseInt(player.style.left) // daca style.left nu exista avem NaN
+    if (oldLeft < 370) {
+      player.style.left = `${oldLeft + 10}px` // NaN + 12 => NaN
+    }
   }
+  // la tema faceti pentru restul directilor
 });
 
 
 // 3.  Un player nu poate iesi inafara gameContainer ( de la punctul 1)
+
+// 4.  Adaugati abstacole, puneti cateva div-uri ( dimensiuni fixe cu anumita culoare) in gameContainer, pot fi random puse in mapa jocuri. 
+// tips 
+const obstaco = {
+  left: Math.random() * 400, // asa generam nr random
+  top: 10
+}
+const obstacolList = [obstaco]
