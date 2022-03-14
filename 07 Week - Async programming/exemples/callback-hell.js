c59c499581cd6fa594d210c5b8dea51583aba822
Razvan Timis <razvan.timis@8x8.com>
2022-03-14 19:47:32 +0200

    Add Co-authored-by: berhecielena <berhecielena@users.noreply.github.com> Co-authored-by: irinaeris <irinaeris@users.noreply.github.com> Co-authored-by: VladTamas20 <VladTamas20@users.noreply.github.com> Co-authored-by: DaniiS97 <DaniiS97@users.noreply.github.com> Co-authored-by: MomeaAlexandru <MomeaAlexandru@users.noreply.github.com> Co-authored-by: Alexandru Țicău <neverbesaved@users.noreply.github.com> Co-authored-by: AndreeaFene <AndreeaFene@users.noreply.github.com> Co-authored-by: Alina <alina-p17@users.noreply.github.com> Co-authored-by: dimusz <dimusz@users.noreply.github.com> Co-authored-by: Radu Ionescu <radionesh@users.noreply.github.com> Co-authored-by: Bogdan Muntean <bogdan-muntean@users.noreply.github.com> Co-authored-by: ClaudiaCristinaTomus <ClaudiaCristinaTomus@users.noreply.github.com>

--- 07 Week - Async programming/exemples/callback-hell.js
+++ 07 Week - Async programming/exemples/callback-hell.js
@@ -15,21 +15,93 @@
 // console.log("getResult() = ", getResultA());
 
 
-function getResultA(callbackResult) {
+function getResultA(fnReturnezResult) {
   // cum putem executa asyncron acest cod
+  let result;
   setTimeout(function () {
     // cod de aici se executa async
-    const result = 10 + 232;
+    result = 10 + 232; // un calcul care dureaza mult
     console.log('sa executat functie async din getResultA')
     // aici deja result este calculat
-    callbackResult(result) // apelam functia callback cu valoare calculata
+    fnReturnezResult(result) // apelam functia callback cu valoare calculata
   }, 0)
+  return result; // valoare ca va fi returna este undefined
   // return result; nu ne ajuta sa returnam in cazul codului async
   // pentru ca return se executa inaite de codul async
 }
 // console.log("getResult() = ", getResultA());
 getResultA(function (resultA) {
   console.log("result de la getResultA = ", resultA);
+  // pe linia 25 se apeleaza acesta functie
 })
 
 // cand avem cod asyncron, avem nevoie de un callback ca scoate rezultatul din functie
+
+function getResultB(resultA, callBackResult) {
+  setTimeout(function () { // cod async 
+    const result = resultA + 1000;
+
+    // sa il scoate inafara
+    callBackResult(result);
+  }, 0);
+}
+
+getResultB(2, function (resultB) {
+  console.log(resultB)
+})
+
+
+// console.log(getResultB()); // functia ar trebui sa returneze o valoare
+// daca avem setTimeout nu se poate pentru ca este cod async
+
+
+getResultA(function (resultA) {
+  getResultB(resultA, function (resultB) {
+    console.log(resultB)
+  })
+})
+
+// Promise
+// Este promisiunea ca ne va da resultul
+function getResultA() {
+  // am vazut ca nu putem folosi direct return 
+  return new Promise(function (peSucces, peErroare) {
+    setTimeout(function () {
+      const result = 10 + 232; // un calcul care dureaza mult
+      peSucces(result);
+    }, 0)
+  })
+}
+
+getResultA()
+  .then(function (resultA) {
+    console.log(resultA)
+  })
+
+
+function getResultB(resultA) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () { // cod async 
+      const result = resultA + 1000;
+      // sa il scoate inafara
+      resolve(result);
+    }, 0);
+  })
+}
+
+getResultA()
+  .then(function (resultA) {
+    return getResultB(resultA);
+  })
+  .then(function (resultB) {
+    console.log(resultB)
+  })
+
+// es6
+// async/await folosite defapt Promise doar este mai elegant la scriere
+async function main() {
+  const resultA = await getResultA();
+  const resultB = await getResultB(resultA);
+
+  console.log(resultB);
+}
