691b2cd9297653572a550dbc3e79859e43539c24
Razvan Timis <razvan.timis@8x8.com>
2022-03-28 19:37:10 +0300

    Add

---
+++ 09 Week - es6/exemples/enhanced-object-literals.js
@@ -0,0 +1,17 @@
+// Enhanced Object Literals
+
+const firstName = 'Alex'
+const lastName = 'M';
+
+// inaite de es6
+const person = {
+  firstName: firstName,
+  lastName: lastName
+}
+
+// dupa es6
+
+const person2 = {
+  firstName, // numele variabilei va fi numele proprietati iar valoare va fi valoare proprietati
+  lastName
+}
