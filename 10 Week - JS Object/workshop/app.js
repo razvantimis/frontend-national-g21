91c9593d1a62ee48f7a8a6fc3dfdda0282d1d0f6
Razvan Timis <razvan.timis@8x8.com>
2022-04-04 18:48:23 +0300

    Add
    Co-authored-by: berhecielena <berhecielena@users.noreply.github.com>
    Co-authored-by: Alina <alina-p17@users.noreply.github.com>
    Co-authored-by: AndreeaFene <AndreeaFene@users.noreply.github.com>
    Co-authored-by: MomeaAlexandru <MomeaAlexandru@users.noreply.github.com>
    Co-authored-by: Alexandru Țicău <neverbesaved@users.noreply.github.com>
    Co-authored-by: dimusz <dimusz@users.noreply.github.com>
    Co-authored-by: ClaudiaCristinaTomus <ClaudiaCristinaTomus@users.noreply.github.com>
    Co-authored-by: Dan Groza <dangroza88@users.noreply.github.com>
    Co-authored-by: Alexandru Mihailescu <malexino@users.noreply.github.com>
    Co-authored-by: irinaeris <irinaeris@users.noreply.github.com>
    Co-authored-by: Bogdan Muntean <bogdan-muntean@users.noreply.github.com>

--- 10 Week - JS Object/workshop/app.js
+++ 10 Week - JS Object/workshop/app.js
@@ -4,9 +4,18 @@ const car1 = new Car(); // se apeleaza functia constructor din clasa Car
 const car2 = new Car();
 const car3 = new Car();
 
-car1.moveRight();
-car1.moveRight();
-car1.moveRight();
-car1.moveRight();
+// car1.moveRight();
+// car1.moveRight();
+// car1.moveRight();
+// car1.moveRight();
 
-car2.moveRight();
+// car2.moveRight();
+
+// Sa ascultam la event de arrow right de la tastatura
+
+document.addEventListener('keydown', function (event) {
+  if (event.code === 'ArrowRight') {
+    console.log('s-a apasat key right')
+    car1.moveRight();
+  }
+})
