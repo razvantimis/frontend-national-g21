66e9d3f4ab28d1be4e0199367a90de418c880103
Razvan Timis <razvan.timis@8x8.com>
2022-04-06 21:05:32 +0300

    Add
    Co-authored-by: Alexandru Țicău <neverbesaved@users.noreply.github.com>
    Co-authored-by: MomeaAlexandru <MomeaAlexandru@users.noreply.github.com>
    Co-authored-by: ClaudiaCristinaTomus <ClaudiaCristinaTomus@users.noreply.github.com>
    Co-authored-by: Alina <alina-p17@users.noreply.github.com>
    Co-authored-by: AndreeaFene <AndreeaFene@users.noreply.github.com>
    Co-authored-by: irinaeris <irinaeris@users.noreply.github.com>
    Co-authored-by: Bogdan Muntean <bogdan-muntean@users.noreply.github.com>

--- 11 Week - OOP/workshop/Player.js
+++ 11 Week - OOP/workshop/Player.js
@@ -1,5 +1,11 @@
 class Player {
+  constructor(container) {
+    let player = document.createElement("div")
+    player.classList.add("player")
 
+    container.appendChild(player)
+
+  }
 }
 
 export default Player;
--- 11 Week - OOP/workshop/app.js
+++ 11 Week - OOP/workshop/app.js
@@ -1,1 +1,6 @@
 import Player from './Player.js'
+
+
+const gameContainer = document.getElementById('game-container')
+
+const player = new Player(gameContainer);
