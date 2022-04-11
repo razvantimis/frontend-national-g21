b1a11ec08841e8828ee36531959510a81eb020f8
Razvan Timis <razvan.timis@8x8.com>
2022-04-11 19:50:26 +0300

    Add Co-authored-by: MomeaAlexandru <MomeaAlexandru@users.noreply.github.com> Co-authored-by: ClaudiaCristinaTomus <ClaudiaCristinaTomus@users.noreply.github.com> Co-authored-by: Alina <alina-p17@users.noreply.github.com> Co-authored-by: AndreeaFene <AndreeaFene@users.noreply.github.com> Co-authored-by: Alexandru Mihailescu <malexino@users.noreply.github.com> Co-authored-by: dimusz <dimusz@users.noreply.github.com> Co-authored-by: Alexandru Țicău <neverbesaved@users.noreply.github.com> Co-authored-by: Denis Cucos <DenisCucos@users.noreply.github.com> Co-authored-by: berhecielena <berhecielena@users.noreply.github.com> Co-authored-by: Bogdan Muntean <bogdan-muntean@users.noreply.github.com> Co-authored-by: Dan Groza <dangroza88@users.noreply.github.com>

--- 11 Week - OOP/workshop/Player.js
+++ 11 Week - OOP/workshop/Player.js
@@ -1,16 +1,51 @@
 class Player {
 
-  static createElement(){
+  static createElement() {
+    // nu avem acces la this
     let player = document.createElement("div")
     player.classList.add("player")
+    player.style.top = '0px';
+    player.style.left = '0px';
 
     return player;
   }
 
   constructor(container) {
-    const playerDiv = Player.createElement();
-    container.appendChild(playerDiv)
+    this.playerDiv = Player.createElement();
+    container.appendChild(this.playerDiv)
+  }
 
+  moveDown() {
+    // aici putem logica de move down
+    const oldTop = parseInt(this.playerDiv.style.top);
+    const newTop = oldTop + 10; // 10px , 20px
+    if (newTop <= 370) {
+      this.playerDiv.style.top = newTop + "px";
+    }
+  }
+
+  moveLeft() { // aici putem logica de move left
+    const oldLeft = parseInt(this.playerDiv.style.left);
+    const newLeft = oldLeft - 10;
+    if (newLeft >= 0) {
+      this.playerDiv.style.left = newLeft + "px";
+    }
+  }
+
+  moveRight() { // aici putem logica de move ri
+    const oldLeft = parseInt(this.playerDiv.style.left);
+    const newLeft = oldLeft + 10;
+    if (newLeft <= 370) {
+      this.playerDiv.style.left = newLeft + "px";
+    }
+  }
+
+  moveUp() { // aici putem logica de move ri
+    const oldTop = parseInt(this.playerDiv.style.top);
+    const newTop = oldTop - 10; // 10px , 20px
+    if (newTop >= 0) {
+      this.playerDiv.style.top = newTop + 'px';
+    }
   }
 }
 
--- 11 Week - OOP/workshop/app.js
+++ 11 Week - OOP/workshop/app.js
@@ -4,3 +4,20 @@ import Player from './Player.js'
 const gameContainer = document.getElementById('game-container')
 
 const player = new Player(gameContainer);
+
+document.addEventListener("keydown", function (eventKeydown) {
+  if (eventKeydown.code === "ArrowDown") {
+    // console.log("keydown", eventKeydown)
+
+    player.moveDown()
+  }
+  if (eventKeydown.code === "ArrowLeft") {
+    player.moveLeft()
+  }
+  if (eventKeydown.code === "ArrowRight") {
+    player.moveRight()
+  }
+  if (eventKeydown.code === "ArrowUp") {
+    player.moveUp()
+  }
+}) 
