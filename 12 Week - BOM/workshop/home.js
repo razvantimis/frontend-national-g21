2196ce17ab22d8c501ecdad2e62f5b12ebf27dea
Razvan Timis <razvan.timis@8x8.com>
2022-04-18 21:07:10 +0300

    Add
    Co-authored-by: MomeaAlexandru <MomeaAlexandru@users.noreply.github.com>
    Co-authored-by: berhecielena <berhecielena@users.noreply.github.com>
    Co-authored-by: dimusz <dimusz@users.noreply.github.com>
    Co-authored-by: Alexandru Țicău <neverbesaved@users.noreply.github.com>
    Co-authored-by: Alina <alina-p17@users.noreply.github.com>
    Co-authored-by: Simona Popa <SimPopa@users.noreply.github.com>
    Co-authored-by: Bogdan Muntean <bogdan-muntean@users.noreply.github.com>
    Co-authored-by: ClaudiaCristinaTomus <ClaudiaCristinaTomus@users.noreply.github.com>
    Co-authored-by: Dan Groza <dangroza88@users.noreply.github.com>
    Co-authored-by: Alexandru Mihailescu <malexino@users.noreply.github.com>

--- 12 Week - BOM/workshop/home.js
+++ 12 Week - BOM/workshop/home.js
@@ -12,9 +12,27 @@ async function getProducts() {
 
 
 getProducts().then(products => {
-  console.log(products);
   createProductListHtml(products)
 })
+
+// Cum arata un cos de produse?
+const shopCart = [
+]
+
+function addProductToCart(product) {
+  const existProduct = shopCart.find(el => el.id === product.id);
+  if (existProduct == undefined) {
+    // daca nu exista product in shopCart atunci facem push cu quanity:1
+    product.quantity = 1;
+    shopCart.push(product);
+  } else {
+    // daca exista atunci trebui sa crestem quanity
+    existProduct.quantity += 1; // nu trebui sa facem push in array pentru ca avem referinta din array
+  }
+
+  console.log(shopCart)
+
+}
 
 function createProductListHtml(listOfProducts) {
   const productContainer = document.querySelector('.products')
@@ -25,10 +43,22 @@ function createProductListHtml(listOfProducts) {
     productHtml.classList.add('product')
     productHtml.innerHTML = `
         <h2>${productValue.title}</h2>
-        <h3>Price: ${productValue.price}</h3>
+        <h3>Price: ${productValue.price} $</h3>
         `
     productContainer.appendChild(productHtml);
+
+    const button = document.createElement("button");
+    button.innerText = "Add to cart";
+    productHtml.appendChild(button);
+    button.onclick = function () {
+      addProductToCart(productValue)
+    }
+
   }
 }
-// const list = [{ title: 'Phone', price: 342 }, { title: 'Phone', price: 342 }, { title: 'Phone', price: 342 }]
+// const list = [
+//   { id: 1, title: 'Phone 1', price: 342 },
+//   { id: 2, title: 'Phone 2', price: 342 },
+//   { id: 3, title: 'Phone 3', price: 342 }
+// ]
 // createProductListHtml(list)
