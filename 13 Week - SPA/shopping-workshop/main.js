5b3942a2c6a795752d15b6d3b5e6b246d19d3eca
Razvan Timis <razvan.timis@8x8.com>
2022-04-20 20:43:03 +0300

    Add
    Co-authored-by: Alexandru Țicău <neverbesaved@users.noreply.github.com>
    Co-authored-by: MomeaAlexandru <MomeaAlexandru@users.noreply.github.com>
    Co-authored-by: berhecielena <berhecielena@users.noreply.github.com>
    Co-authored-by: Alina <alina-p17@users.noreply.github.com>
    Co-authored-by: ClaudiaCristinaTomus <ClaudiaCristinaTomus@users.noreply.github.com>
    Co-authored-by: AndreeaFene <AndreeaFene@users.noreply.github.com>
    Co-authored-by: dimusz <dimusz@users.noreply.github.com>
    Co-authored-by: Dan Groza <dangroza88@users.noreply.github.com>
    Co-authored-by: Bogdan Muntean <bogdan-muntean@users.noreply.github.com>
    Co-authored-by: DaniiS97 <DaniiS97@users.noreply.github.com>

--- 13 Week - SPA/shopping-workshop/main.js
+++ 13 Week - SPA/shopping-workshop/main.js
@@ -1,6 +1,12 @@
 import './style.css'
+import { getProducts, createProductListHtml } from './src/product';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+function showProductList() {
+  getProducts().then(products => {
+    const productContainer = createProductListHtml(products)
+
+    document.querySelector('#app').appendChild(productContainer)
+  })
+}
+
+showProductList();
---
+++ 13 Week - SPA/shopping-workshop/src/product.js
@@ -0,0 +1,49 @@
+import './product.css';
+
+export async function getProducts() {
+  const response = await fetch('https://fakestoreapi.com/products')
+  const products = await response.json();
+  return products;
+}
+
+function addProductToCart(product) {
+  const existProduct = shopCart.find(el => el.id === product.id);
+  if (existProduct == undefined) {
+    // daca nu exista product in shopCart atunci facem push cu quanity:1
+    product.quantity = 1;
+
+    const shopCart = [
+      // { id: 1, title: 'Phone 1', price: 342, quantity: 1 }
+    ]
+    shopCart.push(product);
+  } else {
+    // daca exista atunci trebui sa crestem quanity
+    existProduct.quantity += 1; // nu trebui sa facem push in array pentru ca avem referinta din array
+  }
+}
+
+export function createProductListHtml(listOfProducts) {
+  const productContainer = document.createElement('div')
+  productContainer.classList.add('products')
+  for (let i = 0; i < listOfProducts.length; i++) {
+    const productValue = listOfProducts[i]
+
+    const productHtml = document.createElement("div");
+    productHtml.classList.add('product')
+    productHtml.innerHTML = `
+          <h2>${productValue.title}</h2>
+          <h3>Price: ${productValue.price} $</h3>
+    `
+
+    productContainer.appendChild(productHtml);
+
+    const button = document.createElement("button");
+    button.innerText = "Add to cart";
+    productHtml.appendChild(button);
+    button.onclick = function () {
+      addProductToCart(productValue)
+    }
+  }
+  return productContainer;
+}
+
