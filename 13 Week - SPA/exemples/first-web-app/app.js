4ff05046d793f277cf02936ce2fe0a3d78114936
Razvan Timis <razvan.timis@8x8.com>
2022-04-20 20:20:05 +0300

    Add

---
+++ 13 Week - SPA/exemples/first-web-app/app.js
@@ -0,0 +1,6 @@
+import './app.css'
+
+const box = document.createElement('div');
+box.id = 'box'
+
+document.body.appendChild(box);
--- 13 Week - SPA/exemples/first-web-app/main.js
+++ 13 Week - SPA/exemples/first-web-app/main.js
@@ -1,9 +1,9 @@
 import './style.css'
 import parseInt from 'parse-int';
-
+import './app';
 
 document.querySelector('#app').innerHTML = `
   <h1>Hello Vite!</h1>
-  ${parseInt(10)}
+  ${parseInt(10)} testasdasasdassds
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
