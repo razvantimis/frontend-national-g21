f3e9adbb67843f9ab5de1fd57a2b5b19caf3ccc8
Razvan Timis <razvan.timis@8x8.com>
2022-04-20 19:39:55 +0300

    Add

--- 13 Week - SPA/exemples/first-project/app.js
+++ 13 Week - SPA/exemples/first-project/app.js
@@ -2,3 +2,14 @@ import parseInt from 'parse-int';
 
 console.log(parseInt(10))
 console.log(parseInt("23"))
+
+
+import fetch from 'node-fetch'; // bad practic sa avem importul mai jos
+async function main() {
+  // nu putem folosi fetch doar din node v18
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos')
+  const todos = await response.json();
+  console.log("todos length = ", todos.length)
+}
+
+main();
