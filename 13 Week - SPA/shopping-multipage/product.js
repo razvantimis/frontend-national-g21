930ad00c41e2dc3968859a6826597d3294b4b96e
Razvan Timis <razvan.timis@8x8.com>
2022-04-27 20:39:09 +0300

    Add Co-authored-by: ClaudiaCristinaTomus <ClaudiaCristinaTomus@users.noreply.github.com> Co-authored-by: DaniiS97 <DaniiS97@users.noreply.github.com> Co-authored-by: berhecielena <berhecielena@users.noreply.github.com> Co-authored-by: Alina <alina-p17@users.noreply.github.com> Co-authored-by: Alexandru Țicău <neverbesaved@users.noreply.github.com> Co-authored-by: dimusz <dimusz@users.noreply.github.com> Co-authored-by: Bogdan Muntean <bogdan-muntean@users.noreply.github.com> Co-authored-by: Simona Popa <SimPopa@users.noreply.github.com> Co-authored-by: AndreeaFene <AndreeaFene@users.noreply.github.com> Co-authored-by: Dan Groza <dangroza88@users.noreply.github.com>

--- 13 Week - SPA/shopping-multipage/product.js
+++ 13 Week - SPA/shopping-multipage/product.js
@@ -1,1 +1,16 @@
-const product = { id: 1, title: 'Phone 1', price: 342 }
+// 1. Obtinem id din url ( query params )
+const productID = location.search.substring(4); // "?id=".length
+console.log(productID)
+
+// 2. facem fetch la server dupa informatii
+
+fetch(`https://fakestoreapi.com/products/${productID}`)
+  .then(res => res.json())
+  .then(productDetails => {
+    console.log(productDetails)
+    const itemDetails = document.querySelector('div')
+    itemDetails.innerHTML = `
+      <h2>Title :${productDetails.title}</h2>
+      <img src="${productDetails.image}" style="width: 150px;" />  
+    `
+  })
