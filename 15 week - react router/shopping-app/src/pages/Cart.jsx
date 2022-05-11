94eb4f69e4c1f61ef5a160e0ffea48d97ceead8a
Razvan Timis <razvan.timis@8x8.com>
2022-05-11 20:06:15 +0300

    Add

--- 15 week - react router/shopping-app/src/pages/Cart.jsx
+++ 15 week - react router/shopping-app/src/pages/Cart.jsx
@@ -1,27 +1,24 @@
 
 const shopCart = [
-  { id: 1, title: 'Phone 1', price: 342, quantity: 1 },
-  { id: 2, title: 'Phone 13', price: 34665, quantity: 10 },
+  { id: 1, title: 'IPhone 1', price: 342, quantity: 1 },
+  { id: 2, title: 'IPhone 13', price: 34665, quantity: 10 },
 ]
 
 function Cart() {
+  const shopList = shopCart.map((item, index) => (
+    <li>
+      <h2>{item.title}</h2>
+      <h3>Price: {item.price}</h3>
+      <h4>Quantity:{item.quantity}</h4>
+    </li>
+  ))
   return (
     <div>
       <ul class="shop-list">
-        <li>
-          <h2>Title:'IPhone 1'</h2>
-          <h3>Price: 342</h3>
-          <h4>Quantity:1</h4>
-        </li>
-        <li>
-          <h2>Title:'IPhone 1'</h2>
-          <h3>Price: 342</h3>
-          <h4>Quantity:1</h4>
-        </li>
+        {shopList}
       </ul>
     </div>
   )
-  
 }
 
 export default Cart;
