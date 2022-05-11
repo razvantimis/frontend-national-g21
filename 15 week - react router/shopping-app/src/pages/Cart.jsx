6f06aa12641d889dc0a238d610aa66135518025b
Razvan Timis <razvan.timis@8x8.com>
2022-05-11 19:39:18 +0300

    Add Co-authored-by: MomeaAlexandru <MomeaAlexandru@users.noreply.github.com> Co-authored-by: Alexandru Țicău <neverbesaved@users.noreply.github.com> Co-authored-by: Alina <alina-p17@users.noreply.github.com> Co-authored-by: ClaudiaCristinaTomus <ClaudiaCristinaTomus@users.noreply.github.com> Co-authored-by: Bogdan Muntean <bogdan-muntean@users.noreply.github.com> Co-authored-by: Simona Popa <SimPopa@users.noreply.github.com> Co-authored-by: berhecielena <berhecielena@users.noreply.github.com> Co-authored-by: Dan Groza <dangroza88@users.noreply.github.com> Co-authored-by: AndreeaFene <AndreeaFene@users.noreply.github.com>

--- 15 week - react router/shopping-app/src/pages/Cart.jsx
+++ 15 week - react router/shopping-app/src/pages/Cart.jsx
@@ -1,5 +1,27 @@
+
+const shopCart = [
+  { id: 1, title: 'Phone 1', price: 342, quantity: 1 },
+  { id: 2, title: 'Phone 13', price: 34665, quantity: 10 },
+]
+
 function Cart() {
-  return (<div>Cart</div>)
+  return (
+    <div>
+      <ul class="shop-list">
+        <li>
+          <h2>Title:'IPhone 1'</h2>
+          <h3>Price: 342</h3>
+          <h4>Quantity:1</h4>
+        </li>
+        <li>
+          <h2>Title:'IPhone 1'</h2>
+          <h3>Price: 342</h3>
+          <h4>Quantity:1</h4>
+        </li>
+      </ul>
+    </div>
+  )
+  
 }
 
 export default Cart;
--- 15 week - react router/shopping-app/src/pages/Home.jsx
+++ 15 week - react router/shopping-app/src/pages/Home.jsx
@@ -1,13 +1,44 @@
 import { useState, useEffect } from 'react';
 
 async function getProducts() {
-  const response = await fetch('https://fakestoreapi.com/products')
-  const products = await response.json();
-  return products;
+  // const response = await fetch('https://fakestoreapi.com/products')
+  // const products = await response.json();
+  // return products;
+  return [
+    {
+      title: 'Title 1 produs',
+      price: 134,
+      id: 1,
+    },
+    {
+      title: 'Title 2 produs',
+      price: 342,
+      id: 2,
+    },
+    {
+      title: 'Title 1 produs',
+      price: 134,
+      id: 3,
+    },
+    {
+      title: 'Title 2 produs',
+      price: 342,
+      id: 4,
+    },
+    {
+      title: 'Title 1 produs',
+      price: 134,
+      id: 5,
+    },
+    {
+      title: 'Title 2 produs',
+      price: 342,
+      id: 6,
+    }
+  ]
 }
 
 function Product(props) {
-
   return (
     <div className='product'>
       <h2>{props.title}</h2>
@@ -18,20 +49,28 @@ function Product(props) {
 }
 
 function Home() {
-  const [products, setProducts] = useState([
-  ])
+  const [products, setProducts] = useState([])
 
   useEffect(() => {
     getProducts().then((produse) => {
       setProducts(produse)
     })
-  }, [])
+    console.log('sa vedem')
+  }, []);
 
-  return (<div className='products'>
-    {products.map(product => (
-      <Product title={product.title} price={product.price} />
-    ))}
-  </div>)
+  const arrayOfProductCmps = products.map((product, index) => (
+    <Product
+      key={index}
+      title={product.title}
+      price={product.price}
+    />
+  ));
+
+  return (
+    <div className='products'>
+      {arrayOfProductCmps}
+    </div>
+  )
 }
 
 
